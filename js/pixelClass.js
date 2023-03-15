d02965c8eae8fffd6e84852c1c865813831f3c9d
Dennicemo <92814883+Dennicemo@users.noreply.github.com>
2023-03-15 19:08:53 +0300

    Update pixelClass.js

--- js/pixelClass.js
+++ js/pixelClass.js
@@ -1,7 +1,7 @@
-function getRandomArbitrary(min, max) {
+function getRandomArbitrary(min, max) { // Функция, возвращающая случайное число из равномерного распределения в указанном диапазоне
   return Math.random() * (max - min) + min;
 }
-function getRandomColor() {
+function getRandomColor() { // Генерация случайного числа, собираемого в строке в код цвета
   var letters = '23456789AB';
   var color = '#';
   for (var i = 0; i < 6; i++) {
@@ -10,10 +10,11 @@ function getRandomColor() {
   return color;
 }
 
-lenghts = [11, 9, 7, 5, 3, 1];
-let iterator = 0;
+lenghts = [11, 9, 7, 5, 3, 1]; // Массив "хороших" (нечётных) размеров блоков, из которых можно весьма просто получить пирамиду
 
-function randomOdd(n, m) {
+let iterator = 0; // Отсчёт количества сгенерированных блоков, используется, чтобы далее с вероятностью 0.5 получить блок с хорошей длиной
+
+function randomOdd(n, m) { // Функция, выдающая случайное нечётное из заданного диапазона
   let min, max;
   if (n > m) {
     min = m;
@@ -28,7 +29,7 @@ function randomOdd(n, m) {
   return result + ((result % 2) - 1);
 }
 
-class Block {
+class Block { // Описание класса блока, а именно его конструктора
   constructor() {
     this.colorOfBlock = getRandomColor();
     this.Xcor = 5;
@@ -61,31 +62,31 @@ class Block {
     }
   }
 
-  setBlockInFor(color){
+  setBlockInFor(color){ // Прорисовка блока с заданным цветом
     for (let i = this.Xcor; i < this.Xcor+this.lenght; i++) {
       document.getElementById( '' + this.Ycor + ',' + i).style.backgroundColor=color;
     }
   }
 
   Refresh(){
-    this.setBlockInFor(this.colorOfBlock);
+    this.setBlockInFor(this.colorOfBlock); // Перерисовка блока
   }
 
   Clean(){
-    this.setBlockInFor('#b3d4fc');
+    this.setBlockInFor('#b3d4fc'); // Замена цвета блока на цвет фона - то есть очистка места, где был блок
   }
 
-  GetDown(){
+  GetDown(){ // Перенос координат блока вниз с проверкой дна
     if (this.Ycor<23){
       this.Ycor++;
     }
   }
-  GoLeft(){
+  GoLeft(){ // Перенос координат блока влево с проверкой левой границы
     if (this.Xcor>0 && document.getElementById( '' + (this.Ycor) + ',' + (this.Xcor-1)).style.backgroundColor==='rgb(179, 212, 252)'){
       this.Xcor--;
     }
   }
-  GoRight(){
+  GoRight(){ // Перенос координат блока вправо с проверкой правой границы - возврат False в случае встречи с границей
     //TODO Исправить баг со съеданием при съезжании направо
     if (this.Xcor<23-this.lenght){
       this.Xcor++;
